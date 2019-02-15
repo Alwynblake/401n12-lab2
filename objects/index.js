3f9abb1f0484360e2373df83cbb56f25e812290e
Alistair Blake <consultations@me.com>
2019-02-14 20:09:28 -0800

    carandmotorcyle

---
+++ objects/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+class Vehicle {
+    constructor(name, wheels) {
+        this.name = name;
+        this.wheels = wheels;
+    }
+    drive() {
+       return 'Moving Forward';
+    }
+    stop() {
+       return 'Stopping';
+    }
+}
